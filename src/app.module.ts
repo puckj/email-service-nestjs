ba910a2cb2a51bf340f8bb617024f647534cfd0a
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2023-09-20 15:00:22 +0700

    create scheduledSendMail function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { DailyTaskModule } from './daily-task/daily-task.module';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DailyTaskModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SENDER_TRANSPORT_HOST,
+        port: process.env.SENDER_TRANSPORT_PORT,
+        secure: false,
+        auth: {
+          user: process.env.SENDER_TRANSPORT_EMAIL,
+          pass: process.env.SENDER_TRANSPORT_PASSWORD,
+        },
+      },
+    }),
+    DailyTaskModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/daily-task/daily-task.service.ts
+++ src/daily-task/daily-task.service.ts
@@ -1,20 +1,34 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import * as cron from 'node-cron';
 
 @Injectable()
 export class DailyTaskService {
-  private readonly cronSchedule = process.env.CRON_SCHEDULE
-    ? process.env.CRON_SCHEDULE
-    : '00 00 00 * * *'; //default CRON_SCHEDULE | task run every day at 00:00.00 (everyday)
-
-  constructor() {
-    cron.schedule(this.cronSchedule, () => {
-      this.scheduledSendMail();
-    });
+  constructor(private readonly mailerService: MailerService) {
+    cron.schedule(
+      process.env.CRON_SCHEDULE ? process.env.CRON_SCHEDULE : '00 00 00 * * *', //default CRON_SCHEDULE | task run every day at 00:00.00 (everyday)
+      () => {
+        this.scheduledSendMail();
+      },
+    );
   }
 
   private async scheduledSendMail() {
-    const dateNow = new Date();
-    console.log('Daily task executed at ' + dateNow);
+    console.log('Daily task executed at ' + new Date());
+
+    const emailMessage = {
+      to: process.env.RECIPIENT_EMAIL,
+      subject: process.env.SUBJECT_EMAIL,
+      text: process.env.TEXT_EMAIL,
+    };
+    try {
+      const result = await this.mailerService.sendMail({
+        ...emailMessage,
+        // Additional email options if needed
+      });
+      console.log(result, ' Email sent successfully');
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
   }
 }
